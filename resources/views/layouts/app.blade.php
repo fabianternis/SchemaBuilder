fcb73fc5c6637cc6d2624cf346012ed78cc943eb
Fabian ternis <f.ternis@xpsystems.eu>
2026-07-14 09:16:06 +0200

    feat: Add app.css (UI is kind of useable now)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
-    @yield('head')
+    <link rel="stylesheet" href="/app.css">
+    @yield('head') 
 </head>
 <body>
     @include('components.navbar')
